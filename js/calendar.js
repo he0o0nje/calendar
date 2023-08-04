4f378f3c5d427dae50ae170c3dbcf3d79d1e48e4
내he0o0nje <dudwogjswp@naver.com>
2023-08-04 20:53:36 +0900

    8-4

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,6 @@
-const date = new Date();
-const currYear = date.getFullYear(),
-  currMonth = date.getMonth() + 1;
+let date = new Date();
+let currYear = date.getFullYear(),
+  currMonth = date.getMonth();
 const months = [
   "January",
   "February",
@@ -16,16 +16,37 @@ const months = [
   "December",
 ];
 const currDate = document.querySelector(".month_this");
-const daysTag = document.querySelector('#tbl_month');
+const daysTag = document.querySelector(".days");
+const prevNextIcon = document.querySelectorAll(".month_move");
 
 const renderCalendar = () => {
   currDate.innerHTML = `${months[currMonth]} ${currYear}`;
   let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
-  let tbTag = ''; 
+  let liTag = "";
   for (let i = 1; i <= lastDateofMonth; i++) {
-    tbTag += `<tr>
-    <td class="sun"><a>${i}</a></td>
-</tr>`;}
-daysTag.innerHTML = tbTag;
+    liTag += `<li>${i}</li>`;
+  }
+
+  daysTag.innerHTML = liTag;
+
+  prevNextIcon.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+      renderCalendar();
+    });
+  });
+
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+  let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
+  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+
+  for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class = "hide">${lastDateofLastMonth - i + 1}</li>`;
+  }
+
+  for (let i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class = "hide">${i - lastDayofMonth + 1}</li>`;
+  }
 };
 renderCalendar();
